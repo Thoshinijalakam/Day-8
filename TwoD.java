524142ee1ca03993c183e45dc23263ec5c121d0a
Thoshinijalakam <thoshinijalakam@gmail.com>
2024-12-09 15:22:39 +0530

    Add files via upload

---
+++ TwoD.java
@@ -0,0 +1,30 @@
+ import java.util.*;
+ public class TwoD {
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        int r=sc.nextInt();
+        int c=sc.nextInt();
+        int arr[][]=new int[r][c];
+        for(int i=0;i<r;i++){
+            for(int j=0;j<c;j++){
+                arr[i][j]=sc.nextInt();
+            }
+        }
+        for(int i=0;i<r;i++){
+            for(int j=0;j<c;j++){
+                System.out.print(arr[i][j]+" ");
+            }
+            System.out.println();
+        }
+    }
+    
+}
+/*   2 3
+1    
+2
+3
+4
+5
+6
+1 2 3 
+4 5 6 */
