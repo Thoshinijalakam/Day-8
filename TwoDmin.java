61c13153596463b8266d131b8634b431fa4f9ffd
Thoshinijalakam <thoshinijalakam@gmail.com>
2024-12-09 15:24:30 +0530

    Add files via upload

---
+++ TwoDmin.java
@@ -0,0 +1,33 @@
+import java.util.*;
+ public class TwoDmin
+  {
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        int r=sc.nextInt();
+        int c=sc.nextInt();
+        int arr[][]=new int[r][c];
+        for(int i=0;i<r;i++){
+            for(int j=0;j<c;j++){
+                arr[i][j]=sc.nextInt();
+            }
+        }
+        int min=arr[0][0];
+        for(int i=0;i<r;i++)
+        {
+            for(int j=0;j<c;j++){
+                if(arr[i][j]<min)
+                min=arr[i][j];
+            }
+        }
+        System.out.println(min);
+    }
+ }
+ /*   2 3
+1
+65
+43
+654
+2333
+0
+0 */
+
